de3a5a98714ee7c37ebf94702ea589bf862a23af
Nishan Thapa <47285346+nishan855@users.noreply.github.com>
2021-06-27 11:22:50 -0500

    Delete DynamoConfig.js

--- quickquote/src/DynamoConfig.js
+++
@@ -1,7 +0,0 @@
-let DynamoConfig = {
-    "region": "us-east-2",
-    "endpoint": "http://dynamodb.us-east-2.amazonaws.com",
-    "accessKeyId": "AKIARZGXFZKPTIKMFHW5", "secretAccessKey": "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"
-};
-
-export default DynamoConfig;
