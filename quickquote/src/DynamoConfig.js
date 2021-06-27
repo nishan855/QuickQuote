c9424720a27ad89bb08d88505054e62de69c7f33
Nishan Thapa <nishanthapa855@gmail.om>
2021-06-27 11:14:35 -0500

    read write parameter seller--

---
+++ quickquote/src/DynamoConfig.js
@@ -0,0 +1,7 @@
+let DynamoConfig = {
+    "region": "us-east-2",
+    "endpoint": "http://dynamodb.us-east-2.amazonaws.com",
+    "accessKeyId": "AKIARZGXFZKPTIKMFHW5", "secretAccessKey": "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"
+};
+
+export default DynamoConfig;
