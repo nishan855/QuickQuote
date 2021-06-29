9976118f75fcc425459b212085058683afaab429
Nishan Thapa <nishanthapa855@gmail.om>
2021-06-28 22:28:47 -0500

    seller params

--- quickquote/src/Seller/Parameters.js
+++ quickquote/src/Seller/Parameters.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import '../App.css'
 import {Card,CardBody,CardHeader} from "reactstrap";
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
@@ -10,126 +10,295 @@ import DynamoConfig from "../DynamoConfig";
  let AWS= require("aws-sdk");
 
 
+//
+// function Parameters(){
+//     const[areaC,setareaC]=useState(0);
+//     const[cutC,setCutc]=useState(0);
+//     const[setUpC,setSetUpc]=useState(0);
+//     const[piercePC,setPiercePc]=useState(0);
+//
+//     const[sub, setSub]=useState("");
+//     const[proc,setProc]= useState([]);
+//     const [selected, setSelected] = useState([]);
+//
+//
+//
+//
+//     //process options
+//     const options = [
+//         { value: 'Laser', label: 'Laser' },
+//         { value: 'Waterjet', label: 'Waterjet' },
+//         { value: 'Plasma', label: 'Plasma' },
+//     ];
+//
+//
+//
+//     const handleChange = selectedOption => {
+//         console.log(`Option selected:`, selectedOption);
+//     };
+//
+//     async function submitChange(){
+//
+//         AWS.config.update(DynamoConfig);
+//          let DynamoDB = new AWS.DynamoDB.DocumentClient();
+//
+//         var params = {
+//             TableName: "Test",
+//             Item: {
+//                 "t1":"123",
+//                 //array of process
+//                 "material": [
+//                     //material object 1
+//                     {   "matname":"Aluminium",
+//
+//                         //array of process obj
+//                         "process":[
+//
+//                      //process obj 1
+//                     {
+//                         "procname":"WaterJet",
+//                         "cutCost": "120",
+//                     },
+//                             //process obj 2
+//                     {
+//                         "procname": "Laser",
+//                         "cutCost": "20"
+//                     },
+//
+//                 ]
+//                     },
+//
+//
+//                     //material object 2
+//                     {   "matname":"Copper",
+//                         "process":[
+//
+//                             //process obj 1
+//                             {
+//                                 "procname":"WaterJet",
+//                                 "cutCost": "120",
+//                             },
+//                             //process obj 2
+//                             {
+//                                 "procname": "Laser",
+//                                 "cutCost": "20"
+//                             },
+//
+//                         ]
+//                     }
+//
+//
+//         ]
+//             }
+//         };
+//
+//         DynamoDB.put(params, function (err) {
+//             if (err) {
+//                 console.log(err);
+//             }
+//
+//             //this is put
+//         });
+//
+//
+//         }
+//
+//     // async function submitChange(){
+//     //     const process=[];
+//     //     selected.map((element)=>process.push(element.label));
+//     //
+//     //
+//     //     // await Auth.currentAuthenticatedUser().then((data)=>setSub(data.attributes.sub));
+//     //     // console.log(sub+"got");
+//     //
+//     //     //getting sub as user id
+//     //     const user=  await Auth.currentAuthenticatedUser();
+//     //     console.log(user);
+//     //     const id= user.attributes.sub;
+//     //
+//     //     //writting to DB
+//     //     AWS.config.update(DynamoConfig);
+//     //     let docClient = new AWS.DynamoDB.DocumentClient();
+//     //
+//     //     var input = {
+//     //         "id": id, "areaCost": areaC, "cutCost":cutC, "material":"111","pierceCost":piercePC, "process":process,setupCost:setUpC
+//     //     };
+//     //     var params = {
+//     //         TableName: "Seller",
+//     //         Item:  input
+//     //     };
+//     //     docClient.put(params, function (err, data) {
+//     //
+//     //         if (err) {
+//     //             console.log("users::save::error "+JSON.stringify(err, null, 2));
+//     //         } else {
+//     //             console.log("users::save::success" + JSON.stringify(data, null, 2));
+//     //         }
+//     //     });
+//     // }
+//
+//
+//
+//     return (<Form>
+//
+//         <Card>
+//     <CardHeader style={{background:"#71bbd4"}}>Process Available</CardHeader>
+//     <CardBody>
+//             <MultiSelect
+//                 options={options}
+//                 value={selected}
+//                 onChange={setSelected}
+//                 labelledBy="Select"
+//             />
+//     </CardBody>
+//     </Card>
+//
+//         <Card style={{marginTop:"2%"}}>
+//             <CardHeader style={{background:"#71bbd4"}}>Materials and Costs</CardHeader>
+//             <CardBody>
+//         <FormGroup>
+//             <Label for="exampleEmail">Material Cost</Label>
+//             <input className="mb-4" type="number" required value={areaC}
+//                    onChange={(e) => setareaC(e.target.value)}
+//                    />
+//         </FormGroup>
+//
+//         <FormGroup>
+//             <Label for="exampleEmail">Cut Cost</Label>
+//             <input className="mb-4" type="number" required value={cutC}
+//                    onChange={(e) => setCutc(e.target.value)}
+//                    />
+//         </FormGroup>
+//
+//         <FormGroup>
+//             <Label for="exampleEmail">Pierce Point Cost</Label>
+//             <input className="mb-4" type="number" required value={piercePC}
+//                    onChange={(e) => setPiercePc(e.target.value)}
+//                    />
+//         </FormGroup>
+//
+//         <FormGroup>
+//             <Label for="exampleEmail">Set up Cost</Label>
+//             <input className="mb-4" type="number" required value={setUpC}
+//                    onChange={(e) => setSetUpc(e.target.value)}
+//                    />
+//         </FormGroup>
+//
+//             </CardBody>
+//         </Card>
+//
+//         <Button onClick={submitChange}>Submit</Button>
+//
+//     </Form>);
+//
+//
+// }
+//  export default  Parameters;
+//
+//
+//
 
-function Parameters(){
-    const[areaC,setareaC]=useState(0);
-    const[cutC,setCutc]=useState(0);
-    const[setUpC,setSetUpc]=useState(0);
-    const[piercePC,setPiercePc]=useState(0);
-
-    const[sub, setSub]=useState("");
-    const[proc,setProc]= useState([]);
-    const [selected, setSelected] = useState([]);
 
 
 
 
-    //process options
-    const options = [
-        { value: 'Laser', label: 'Laser' },
-        { value: 'Waterjet', label: 'Waterjet' },
-        { value: 'Plasma', label: 'Plasma' },
-    ];
+const Parameters = () => {
 
+    const [inputList, setInputList] = useState([]);
+    const Input = () => {
+        const [processList, setProcessList] = useState([]);
 
+        const Process =()=>{
+            return(
+                <Card>
+                <CardHeader style={{background:"#97CAEF"}}> Process {processList.length+1} Available </CardHeader>
+                <Form>
+                    <FormGroup>
+                        <Label for="exampleEmail">Process Name</Label>
+                        <input placeholder="input Process Name" />
+                    </FormGroup>
 
-    const handleChange = selectedOption => {
-        console.log(`Option selected:`, selectedOption);
+                    <FormGroup>
+                        <Label for="exampleEmail">Price per Inch</Label>
+                        <input type={"number"} placeholder="input price $/inch" />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="exampleEmail">Price per Pierce</Label>
+                        <input  type={"number"} placeholder="input price $/ Pierce" />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="exampleEmail">Set up Cost</Label>
+                        <input type={"number"} placeholder="input Set up Cost" />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="exampleEmail">Process Kurf</Label>
+                        <input placeholder="input Process Kurf" />
+                    </FormGroup>
+
+                    </Form>
+                </Card>
+            )
+        }
+
+        function onProcessClick() {
+            setProcessList(processList.concat(<Process key={processList.length} />));
+        }
+
+        return(
+            <Card>
+            <CardHeader  style={{background:"#71bbd4"}}>Material {inputList.length+1} Setup </CardHeader>
+
+               <FormGroup>
+                 <Label for="exampleEmail">Material Name</Label>
+                <input placeholder="input Material Name" />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="exampleEmail">Material Density</Label>
+                    <input type={"number"} placeholder="input Material Density " />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="exampleEmail">Material Thickness</Label>
+                    <input type={"number"} placeholder="input Material Thickness" />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="exampleEmail">Max Sizes</Label>
+                    <input type={"number"} placeholder="input Max sizes" />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="exampleEmail">Material Cost</Label>
+                    <input type={"number"} placeholder="Input Material Cost" />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="exampleEmail">Lead In/ Lead Out distances</Label>
+                    <input type={"number"} placeholder="input Lead in/out distances" />
+                </FormGroup>
+
+                <button onClick={onProcessClick}>Add Process</button>
+                {processList}
+                </Card>
+        )
     };
 
 
 
-    async function submitChange(){
-        const process=[];
-        selected.map((element)=>process.push(element.label));
+    const onAddBtnClick = event => {
+        setInputList(inputList.concat(<Input key={inputList.length} />));
+    };
 
-
-        // await Auth.currentAuthenticatedUser().then((data)=>setSub(data.attributes.sub));
-        // console.log(sub+"got");
-
-        //getting sub as user id
-        const user=  await Auth.currentAuthenticatedUser();
-        console.log(user);
-        const id= user.attributes.sub;
-
-        //writting to DB
-        AWS.config.update(DynamoConfig);
-        let docClient = new AWS.DynamoDB.DocumentClient();
-
-        var input = {
-            "id": id, "areaCost": areaC, "cutCost":cutC, "material":"111","pierceCost":piercePC, "process":process,setupCost:setUpC
-        };
-        var params = {
-            TableName: "Seller",
-            Item:  input
-        };
-        docClient.put(params, function (err, data) {
-
-            if (err) {
-                console.log("users::save::error "+JSON.stringify(err, null, 2));
-            } else {
-                console.log("users::save::success" + JSON.stringify(data, null, 2));
-            }
-        });
-    }
-
-
-
-    return (<Form>
-
-        <Card>
-    <CardHeader style={{background:"#71bbd4"}}>Process Available</CardHeader>
-    <CardBody>
-            <MultiSelect
-                options={options}
-                value={selected}
-                onChange={setSelected}
-                labelledBy="Select"
-            />
-    </CardBody>
-    </Card>
-
-        <Card style={{marginTop:"2%"}}>
-            <CardHeader style={{background:"#71bbd4"}}>Materials and Costs</CardHeader>
-            <CardBody>
-        <FormGroup>
-            <Label for="exampleEmail">Material Cost</Label>
-            <input className="mb-4" type="number" required value={areaC}
-                   onChange={(e) => setareaC(e.target.value)}
-                   />
-        </FormGroup>
-
-        <FormGroup>
-            <Label for="exampleEmail">Cut Cost</Label>
-            <input className="mb-4" type="number" required value={cutC}
-                   onChange={(e) => setCutc(e.target.value)}
-                   />
-        </FormGroup>
-
-        <FormGroup>
-            <Label for="exampleEmail">Pierce Point Cost</Label>
-            <input className="mb-4" type="number" required value={piercePC}
-                   onChange={(e) => setPiercePc(e.target.value)}
-                   />
-        </FormGroup>
-
-        <FormGroup>
-            <Label for="exampleEmail">Set up Cost</Label>
-            <input className="mb-4" type="number" required value={setUpC}
-                   onChange={(e) => setSetUpc(e.target.value)}
-                   />
-        </FormGroup>
-
-            </CardBody>
-        </Card>
-
-        <Button onClick={submitChange}>Submit</Button>
-
-    </Form>);
-
-
-}
- export default  Parameters;
-
-
+    return (
+        <div>
+            <button onClick={onAddBtnClick}>Add Material</button>
+            {inputList}
+        </div>
+    );
+};
+export default Parameters;
 
