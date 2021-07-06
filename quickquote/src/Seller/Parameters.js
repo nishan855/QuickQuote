e985bda91ed71ee7683fc15ed4cb57efe3989b47
Nishan Thapa <nishanthapa855@gmail.om>
2021-07-06 10:58:33 -0500

    nc

--- quickquote/src/Seller/Parameters.js
+++ quickquote/src/Seller/Parameters.js
@@ -4,213 +4,37 @@ import '../button.css'
 import  {Card,CardBody,CardHeader} from "reactstrap";
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import { Auth } from '@aws-amplify/auth';
-import MultiSelect from "react-multi-select-component";
 import DynamoConfig from "../DynamoConfig";
-import {Toast, ToastBody} from "react-bootstrap";
+import SellerNavbar from "./SellerCompo/SellerNavbar";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 let AWS= require("aws-sdk");
-
-
-//
-//function Parametes() {
-
-
-//}
-
-//
-//
-//
-//
-//     //process options
-//     const options = [
-//         { value: 'Laser', label: 'Laser' },
-//         { value: 'Waterjet', label: 'Waterjet' },
-//         { value: 'Plasma', label: 'Plasma' },
-//     ];
-//
-//
-//
-//     const handleChange = selectedOption => {
-//         console.log(`Option selected:`, selectedOption);
-//     };
-//
-//     async function submitChange(){
-//
-//         AWS.config.update(DynamoConfig);
-//          let DynamoDB = new AWS.DynamoDB.DocumentClient();
-//
-//         var params = {
-//             TableName: "Test",
-//             Item: {
-//                 "t1":"123",
-//                 //array of process
-//                 "material": [
-//                     //material object 1
-//                     {   "matname":"Aluminium",
-//
-//                         //array of process obj
-//                         "process":[
-//
-//                      //process obj 1
-//                     {
-//                         "procname":"WaterJet",
-//                         "cutCost": "120",
-//                     },
-//                             //process obj 2
-//                     {
-//                         "procname": "Laser",
-//                         "cutCost": "20"
-//                     },
-//
-//                 ]
-//                     },
-//
-//
-//                     //material object 2
-//                     {   "matname":"Copper",
-//                         "process":[
-//
-//                             //process obj 1
-//                             {
-//                                 "procname":"WaterJet",
-//                                 "cutCost": "120",
-//                             },
-//                             //process obj 2
-//                             {
-//                                 "procname": "Laser",
-//                                 "cutCost": "20"
-//                             },
-//
-//                         ]
-//                     }
-//
-//
-//         ]
-//             }
-//         };
-//
-//         DynamoDB.put(params, function (err) {
-//             if (err) {
-//                 console.log(err);
-//             }
-//
-//             //this is put
-//         });
-//
-//
-//         }
-//
-//     // async function submitChange(){
-//     //     const process=[];
-//     //     selected.map((element)=>process.push(element.label));
-//     //
-//     //
-//     //     // await Auth.currentAuthenticatedUser().then((data)=>setSub(data.attributes.sub));
-//     //     // console.log(sub+"got");
-//     //
-//     //     //getting sub as user id
-//     //     const user=  await Auth.currentAuthenticatedUser();
-//     //     console.log(user);
-//     //     const id= user.attributes.sub;
-//     //
-//     //     //writting to DB
-//     //     AWS.config.update(DynamoConfig);
-//     //     let docClient = new AWS.DynamoDB.DocumentClient();
-//     //
-//     //     var input = {
-//     //         "id": id, "areaCost": areaC, "cutCost":cutC, "material":"111","pierceCost":piercePC, "process":process,setupCost:setUpC
-//     //     };
-//     //     var params = {
-//     //         TableName: "Seller",
-//     //         Item:  input
-//     //     };
-//     //     docClient.put(params, function (err, data) {
-//     //
-//     //         if (err) {
-//     //             console.log("users::save::error "+JSON.stringify(err, null, 2));
-//     //         } else {
-//     //             console.log("users::save::success" + JSON.stringify(data, null, 2));
-//     //         }
-//     //     });
-//     // }
-//
-//
-//
-//     return (<Form>
-//
-//         <Card>
-//     <CardHeader style={{background:"#71bbd4"}}>Process Available</CardHeader>
-//     <CardBody>
-//             <MultiSelect
-//                 options={options}
-//                 value={selected}
-//                 onChange={setSelected}
-//                 labelledBy="Select"
-//             />
-//     </CardBody>
-//     </Card>
-//
-//         <Card style={{marginTop:"2%"}}>
-//             <CardHeader style={{background:"#71bbd4"}}>Materials and Costs</CardHeader>
-//             <CardBody>
-//         <FormGroup>
-//             <Label for="exampleEmail">Material Cost</Label>
-//             <input className="mb-4" type="number" required value={areaC}
-//                    onChange={(e) => setareaC(e.target.value)}
-//                    />
-//         </FormGroup>
-//
-//         <FormGroup>
-//             <Label for="exampleEmail">Cut Cost</Label>
-//             <input className="mb-4" type="number" required value={cutC}
-//                    onChange={(e) => setCutc(e.target.value)}
-//                    />
-//         </FormGroup>
-//
-//         <FormGroup>
-//             <Label for="exampleEmail">Pierce Point Cost</Label>
-//             <input className="mb-4" type="number" required value={piercePC}
-//                    onChange={(e) => setPiercePc(e.target.value)}
-//                    />
-//         </FormGroup>
-//
-//         <FormGroup>
-//             <Label for="exampleEmail">Set up Cost</Label>
-//             <input className="mb-4" type="number" required value={setUpC}
-//                    onChange={(e) => setSetUpc(e.target.value)}
-//                    />
-//         </FormGroup>
-//
-//             </CardBody>
-//         </Card>
-//
-//         <Button onClick={submitChange}>Submit</Button>
-//
-//     </Form>);
-//
-//
-// }
-//  export default  Parameters;
-//
-//
-//
-
-
-
-
-
-
 
 
 const Parameters = () => {
 
     const val=[];
     let count=0;
+    let savedpr = false;
     const InnerParams = () => {
         let proCount = 0;
-        let savedpr = false;
         const [proceError,setProcErr] = useState(false);
+
+
+            const notify = () => {
+                toast.error('Please save or add  the process first!!', {
+                    position: "bottom-right",
+                    autoClose: 2500,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+
+            }
 
         const [mat, setMat] = useState({
             "matname": "",
@@ -230,13 +54,11 @@ const Parameters = () => {
 
 
         const [inputList, setInputList] = useState([]);
-        //const [processList, setProcessList] = useState([]);
 
 
         const Input = () => {
             const [processList, setProcessList] = useState([]);
 
-            savedpr = false;
 
             const Process = () => {
                 const [processObj, setProcObj] = useState({
@@ -257,7 +79,7 @@ const Parameters = () => {
 
                     }));
                     setSpBtn(true)
-                    savedpr = true;
+                    savedpr=true;
                     console.log(savedpr)
                 }
 
@@ -347,13 +169,14 @@ const Parameters = () => {
 
 
             function onProcessClick() {
-                console.log(savedpr)
-                console.log(proCount)
                 if (savedpr || (proCount == 0)) {
                     setProcessList(processList.concat(<Process key={processList.length}/>));
                     proCount++;
-                    setProcErr(false)
-                } else setProcErr(true)
+                    savedpr=false
+                } else {
+                    notify()
+                    console.log("n1 called")
+                }
 
             }
 
@@ -456,22 +279,33 @@ const Parameters = () => {
                         <Button title="click to add process" size="sm"
                                 style={{background: '#9DC88D', marginBottom: '2%'}} onClick={onProcessClick}>Add Process
                         </Button>
+
                     </Card>
+
                 </Card>
             )
         };
 
 
         const onAddBtnClick = event => {
-            if (count != 0) {
+
+            console.log(count+" "+savedpr)
+            if (count !=0 && !savedpr){
+                console.log("n2 called")
+                notify()
+            }
+            if (count != 0 && savedpr) {
                 val.push(mat);
                 console.log(val);
+                savedpr = false;
+                setInputList(<Input key={inputList.length}/>);
             }
+
+           if(count==0){
+               setInputList(<Input key={inputList.length}/>);
+           }
+
             count++;
-            savedpr = 0;
-
-
-            setInputList(<Input key={inputList.length}/>);
 
 
         };
@@ -508,9 +342,14 @@ const Parameters = () => {
 
 
         const onsaveClick = () => {
-            val.push(mat);
-            console.log(val);
-            submitChange()
+            if (count !=0 && !savedpr){
+                notify()
+            }
+            else {
+                val.push(mat);
+                console.log(val);
+                submitChange()
+            }
         };
 
 
@@ -519,11 +358,23 @@ const Parameters = () => {
 
                 {inputList}
                 <div>
+                    <ToastContainer
+                        position="bottom-right"
+                        autoClose={2500}
+                        hideProgressBar={false}
+                        newestOnTop={false}
+                        closeOnClick
+                        rtl={false}
+                        pauseOnFocusLoss
+                        draggable
+                        pauseOnHover
+                    />
+
                     <button className="button" title="click to add more materials"
                             style={{background: "#71bbd4", float: "left", marginTop: "2%"}} onClick={onAddBtnClick}>Add
                         Material (+)
                     </button>
-                    <button class="savebutton" style={{float: "right"}}
+                    <button class="savebutton"
                             onClick={(event) => onsaveClick(event)}> Submit
                     </button>
                 </div>
@@ -533,6 +384,7 @@ const Parameters = () => {
 
         return(
             <div>
+                <SellerNavbar/>
             <InnerParams/>
             </div>
         );
