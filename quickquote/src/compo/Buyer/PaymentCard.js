a73d146db42fcb9ad1d3c380f85acc3d0694bf0e
Laith <lmarzouq96@gmail.com>
2021-07-13 10:27:58 -0500

    Payment card

---
+++ quickquote/src/compo/Buyer/PaymentCard.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import Cards from 'react-credit-cards';
+import 'react-credit-cards/es/styles-compiled.css';
+
+export default class PaymentCard extends React.Component {
+    state = {
+        cvc: '',
+        expiry: '',
+        focus: '',
+        name: '',
+        number: '',
+    };
+
+    handleInputFocus = (e) => {
+        this.setState({ focus: e.target.name });
+    }
+
+    handleInputChange = (e) => {
+        const { name, value } = e.target;
+
+        this.setState({ [name]: value });
+    }
+
+    render() {
+        return (
+            <div id="PaymentForm">
+                <Cards
+                    cvc={this.state.cvc}
+                    expiry={this.state.expiry}
+                    focused={this.state.focus}
+                    name={this.state.name}
+                    number={this.state.number}
+                />
+                <form>
+                    <input
+                        type="tel"
+                        name="number"
+                        placeholder="Card Number"
+                        onChange={this.handleInputChange}
+                        onFocus={this.handleInputFocus}
+                    />
+                </form>
+                <form>
+                    <input
+                        type="tel"
+                        name="name"
+                        placeholder="John Doe"
+                        onChange={this.handleInputChange}
+                        onFocus={this.handleInputFocus}
+                    />
+                </form>
+                <form>
+                    <input
+                        type="tel"
+                        name="expiry"
+                        placeholder="MM/YYYY"
+                        onChange={this.handleInputChange}
+                        onFocus={this.handleInputFocus}
+                    />
+                </form>
+            </div>
+        );
+    }
+}
