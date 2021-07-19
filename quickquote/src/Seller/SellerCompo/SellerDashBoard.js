3a462a64738edc590bc34705bb380197bc162c4d
Sindhu-Parajuli <57790709+See-in-doo99@users.noreply.github.com>
2021-07-19 16:19:58 -0500

    "Setup material will now go to add material and submit stuff"

--- quickquote/src/Seller/SellerCompo/SellerDashBoard.js
+++ quickquote/src/Seller/SellerCompo/SellerDashBoard.js
@@ -5,7 +5,7 @@ import SellerSidebar from "./SellerSidebar";
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Setup from "./pages/Setup";
 import Metrics from "./pages/Metrics"
-import Parameter from "./Parameters"
+import Parameters from "./Parameters"
 
 
 function SellerDashBoard() {
@@ -17,8 +17,8 @@ function SellerDashBoard() {
                 <SellerSidebar />
                     <Switch>
                         <Route path = '/metrics' exact component = {Metrics} />
-                        <Route path = '/setup' exact component = {Setup} />
-                        <Route path = "/changeParameter" exact component = {Parameter} />
+                        <Route path = '/setup' exact component = {Parameters} />
+                        <Route path = "/changeParameter" exact component = {Parameters} />
                     </Switch>
                 </div>
             </Router>
