7342dcc3e794a93bfcb288d196861b2db1539cca
Nishan Thapa <nishanthapa855@gmail.om>
2021-07-20 21:12:09 -0500

    parameters on BE

---
+++ springPractice/src/main/java/com/DataLayer/DynamoDbconfig.java
@@ -0,0 +1,29 @@
+package com.DataLayer;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDbconfig {
+
+    @Bean
+    public DynamoDBMapper getmapper() {
+        AWSStaticCredentialsProvider acp = new AWSStaticCredentialsProvider(
+                new BasicAWSCredentials("", ""));
+
+        AmazonDynamoDB db= AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(acp)
+                .withRegion(Regions.US_EAST_2).build();
+
+        return  new DynamoDBMapper(db);
+
+
+    }
+
+}
