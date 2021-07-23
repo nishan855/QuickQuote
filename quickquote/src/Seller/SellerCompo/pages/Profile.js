b78d5d555d9262404feb5273d0f16cf2cc52a369
Sindhu-Parajuli <57790709+See-in-doo99@users.noreply.github.com>
2021-07-23 12:02:38 -0500

    "s3 bucket stuff-private"

--- quickquote/src/Seller/SellerCompo/pages/Profile.js
+++ quickquote/src/Seller/SellerCompo/pages/Profile.js
@@ -1,17 +1,25 @@
 import { useState, useEffect } from 'react'
 import { Storage } from 'aws-amplify'
 import S3config from "../../../S3config";
+import { Auth } from '@aws-amplify/auth';
 
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3({'region': 'us-east-2'});
-
+//var identityId = credentialsProvider.getIdentityId();
+var identityId = AWS.config.credentials.identityId;
+//const user = Auth.currentAuthenticatedUser();
+//console.log(user);
+//const id = user.attributes.sub;
 function Profile() {
     const [images, setImages] = useState([])
     useEffect(() => {
         fetchImages()
     }, [])
     async function fetchImages() {
-        let imageKeys = await Storage.list('')
+        let imageKeys = await Storage.list('', {
+            level: 'private',
+            identityId: identityId // the identityId of that user
+        })
         try{
             imageKeys = await Promise.all(imageKeys.map(async k => {
                 const key = await Storage.get(k.key)
@@ -29,7 +37,10 @@ function Profile() {
     async function onChange(e) {
         const file = e.target.files[0];
         try {
-            const result = await Storage.put(file.name, file)
+            const result = await Storage.put(file.name, file,
+                {
+                    level : 'private'
+                })
 
             console.log({result})
             fetchImages()
