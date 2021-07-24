592e2460bdc3cb6307252c414a0a741a94d50239
Nishan Thapa <nishanthapa855@gmail.om>
2021-07-24 15:55:58 -0500

    order....

--- quickquote/src/BaseApp.js
+++ quickquote/src/BaseApp.js
@@ -2,7 +2,7 @@ import React from 'react'
 // import Navbar from './compo/Navbar'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'    // 'npm install react-router-dom'
 import './BaseApp.css'
-
+import QuoteError from "./pages/QuoteError";
 import Home from './pages/Home';
 import Buyer from './pages/Buyer';
 import Seller from "./Seller/SellerCompo/SellerDashBoard";
@@ -17,15 +17,11 @@ function BaseApp() {
           <Switch>
             <Route path = '/' exact component = {Home} />
               <Route path = '/buyers' exact component = {Buyers} />
-
              <Route path = '/quote'  component = {Orders} />
-
+              <Route path = '/quoteerror'  component ={QuoteError} />
             <Route path = '/buyer/:id'  component = {Buyer} />
             <Route path = '/seller' exact component = {Seller} />
               <Route path = '/sellerDashboard' exact component = {Seller} />
-              //<Route path = '/buyer/:id' component = {BuyerHomepage}
-              />
-
 
 
           </Switch>
