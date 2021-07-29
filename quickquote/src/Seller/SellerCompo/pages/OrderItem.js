1254bc7f2a9dd41c8d6bf7f5b969d41e3c973540
Nishan Thapa <nishanthapa855@gmail.om>
2021-07-29 00:15:15 -0500

    paramters fixed

--- quickquote/src/Seller/SellerCompo/pages/OrderItem.js
+++ quickquote/src/Seller/SellerCompo/pages/OrderItem.js
@@ -1,11 +1,27 @@
 import {useLocation} from "react-router-dom";
+import {Card, CardText, CardBody, CardLink, CardTitle, CardSubtitle
+} from 'reactstrap';
+import React from "react";
 
 export default function OrderItem(){
 
     const location=useLocation()
     const orderDat=location.state.ordrData
-    console.log(orderDat)
+
     return(
-        <h1/>
+        <div>
+            <Card>
+                <CardBody>
+                    <CardTitle tag="h5">Order ID: {orderDat.orderId}</CardTitle>
+                    <CardSubtitle tag="h6" className="mb-2 text-muted">{orderDat.orderDate}</CardSubtitle>
+                </CardBody>
+
+                <CardBody>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <CardLink href="#">Card Link</CardLink>
+                    <CardLink href="#">Another Link</CardLink>
+                </CardBody>
+            </Card>
+        </div>
     )
 }
