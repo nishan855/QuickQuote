61e087da0e3bf9b843dc9550e3c2147f4e9314e2
Nishan Thapa <nishanthapa855@gmail.om>
2021-07-30 00:10:18 -0500

    orderitem

--- quickquote/src/Seller/SellerCompo/pages/OrderItem.js
+++ quickquote/src/Seller/SellerCompo/pages/OrderItem.js
@@ -1,27 +1,39 @@
 import {useLocation} from "react-router-dom";
-import {Card, CardText, CardBody, CardLink, CardTitle, CardSubtitle
+import {Card, CardText, CardBody,CardHeader, CardLink, CardTitle, CardSubtitle
 } from 'reactstrap';
 import React from "react";
+import GetAppIcon from '@material-ui/icons/GetApp';
 
 export default function OrderItem(){
 
     const location=useLocation()
     const orderDat=location.state.ordrData
+    console.log(orderDat)
 
     return(
         <div>
-            <Card>
-                <CardBody>
-                    <CardTitle tag="h5">Order ID: {orderDat.orderId}</CardTitle>
-                    <CardSubtitle tag="h6" className="mb-2 text-muted">{orderDat.orderDate}</CardSubtitle>
-                </CardBody>
+            <CardHeader style={{marginTop: "2%", marginBottom:"2%", background:'#14A76c'}}>
+                <CardTitle tag="h5">Order ID: <label style={{color:"blue"}}>{orderDat.orderId}</label></CardTitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">{orderDat.orderDate}</CardSubtitle>
+            </CardHeader>
 
-                <CardBody>
-                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                    <CardLink href="#">Card Link</CardLink>
-                    <CardLink href="#">Another Link</CardLink>
-                </CardBody>
+            {orderDat.data.map((ord,ind)=>
+                <Card key={ind}>
+                    <CardHeader style={{background:"#3FEEE6"}}>{ord.fileKey}<GetAppIcon style={{float:"right"}}/></CardHeader>
+                    <CardBody>
+                        <CardText> Quantity: {ord.quantity}</CardText>
+                        <CardText>Material: {ord.material}</CardText>
+                        <CardText>Process: {ord.process}</CardText>
+                        <CardText>Cost: ${ord.cost}</CardText>
+                    </CardBody>
+                </Card>
+
+            )}
+
+            <Card>
+                <CardHeader style={{marginTop: "2%"}}>Customer Details</CardHeader>
             </Card>
+
         </div>
     )
 }
