8bffb45bce15906d1e67cb13e13383d4605be974
erickeng <ekeng92@gmail.com>
2021-08-05 20:04:28 -0500

    uncommented code in DisplayBuyer. entire "async function fetch()" is causing app to crash.

--- quickquote/src/pages/DisplayBuyer.js
+++ quickquote/src/pages/DisplayBuyer.js
@@ -10,7 +10,6 @@ import {useParams, useHistory} from "react-router-dom";
 
 import {Card, CardBody, CardHeader} from "reactstrap";
 import {Button, Form, FormGroup, Label, Input, FormText} from 'reactstrap';
-
 import "./DisplayBuyer.css"
 
 let AWS = require("aws-sdk");
@@ -21,49 +20,48 @@ function DisplayBuyer() {
     const [motto, setmotto] = useState("");
     const [ids, setid] = useState("");
     const userParam = useParams()
-    //
-    // async function fetch() {
-    //
-    //     AWS.config.update(DynamoConfig);
-    //     //let docClient = new AWS.DynamoDB.DocumentClient();
-    //     //const user=  await Auth.currentAuthenticatedUser();
-    //     //console.log(user);
-    //     const id = userParam.id;
-    //
-    //     //connecting  to db to look for record with primary key
-    //     let docClient = new AWS.DynamoDB.DocumentClient();
-    //
-    //     var params = {
-    //         TableName: "SellerProfile",
-    //         Key: {
-    //             "sellerid": id
-    //         }
-    //     };
-    //
-    //     await docClient.get(params, function (err, data) {
-    //         if (err) {
-    //             console.log("users::fetchOneByKey::error - " + JSON.stringify(err, null, 2));
-    //         } else {
-    //
-    //             setname(data.Item.info[0].cname)
-    //             setmotto(data.Item.info[0].cmotto)
-    //             setid(id)
-    //
-    //         }
-    //     })
-    // }
-    //
-    // //const show = display.map((dt,i) =>
-    //
-    // //<h3>{dt.cname}</h3>
-    // console.log(ids);
-    // //)
-    // //console.log(display.cname)
-    // useEffect(() => fetch(), []);
-    // //console.log(data.item.cname);
+
+    async function fetch() {
+
+        AWS.config.update(DynamoConfig);
+        //let docClient = new AWS.DynamoDB.DocumentClient();
+        //const user=  await Auth.currentAuthenticatedUser();
+        //console.log(user);
+        const id = userParam.id;
+
+        //connecting  to db to look for record with primary key
+        let docClient = new AWS.DynamoDB.DocumentClient();
+
+        var params = {
+            TableName: "SellerProfile",
+            Key: {
+                "sellerid": id
+            }
+        };
+
+        await docClient.get(params, function (err, data) {
+            if (err) {
+                console.log("users::fetchOneByKey::error - " + JSON.stringify(err, null, 2));
+            } else {
+
+                setname(data.Item.info[0].cname)
+                setmotto(data.Item.info[0].cmotto)
+                setid(id)
+
+            }
+        })
+    }
+
+    //const show = display.map((dt,i) =>
+
+    //<h3>{dt.cname}</h3>
+    console.log(ids);
+    //)
+    //console.log(display.cname)
+    useEffect(() => fetch(), []);
+    //console.log(data.item.cname);
 
     return (
-
         <div className = 'logo-motto-container'>
             <Card className = 'logo-card' >
                 <div className="text-center">
@@ -83,11 +81,7 @@ function DisplayBuyer() {
                 </div>
             </Card>
         </div>
-
-
     );
-
-
 }
 
 export default DisplayBuyer;
